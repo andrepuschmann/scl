6a60981b52c9afc580943f27806517723d727009
Andre Puschmann <andre.puschmann@tu-ilmenau.de>
2013-05-31 15:21:39 +0200

    add setup.py script

---
+++ src/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+  name='scl',
+  version = "0.1",
+  py_modules=['scl'],
+  
+  install_requires=[
+    "pyyaml",
+  ],
+    
+  author='Andre Puschmann',
+  author_email='andre.puschmann@tu-ilmenau.de',
+  url = "https://github.com/MOBICOM/scl",
+  license = "GPL",
+)
